7c97d5bfd8077952c71a776490414434e3924b5b
ainzzorl <ainzzorl@gmail.com>
2022-04-24 12:57:19 -0400

    Backup both to Dropbox and iCloud

--- backup-keepass.sh
+++ backup-keepass.sh
@@ -1,3 +1,5 @@
 datestr=$(date '+%Y-%m-%d-%H-%M-%S')
-dest="$HOME/Dropbox/backups/keepass/lac-$datestr.kdbx"
-cp "$HOME/Dropbox/lac.kdbx" $dest
+dest_dropbox="$HOME/Dropbox/backups/keepass/lac-$datestr.kdbx"
+cp "$HOME/Dropbox/lac.kdbx" $dest_dropbox
+dest_icloud="$HOME/Library/Mobile Documents/com~apple~CloudDocs/backups/keepass/lac-$datestr.kdbx"
+cp "$HOME/Dropbox/lac.kdbx" "$dest_icloud"
